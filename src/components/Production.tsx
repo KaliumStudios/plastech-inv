cac9918f461f379d47f8262f8dc6d79c86c245c8
DoggedFilly0 <A01366287@itesm.mx>
2022-10-10 20:30:29 -0500

    Created the data fot the production screen

--- src/components/Production.tsx
+++ src/components/Production.tsx
@@ -1,29 +1,97 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import { TextField, Stack, Card, Typography, Checkbox } from "@mui/material";
+import {
+  cardBackgroundColor,
+  boxMargins,
+  typographyStyles,
+  checkboxesStyles,
+  centeredGrids,
+} from "../styles/Common.styles";
 
-const CardItemText = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#0d1b2a",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: "white",
-  display: "block",
-}));
+// Se utilizaria en caso que el color de borde en los inputs se quiera cambiar de color
+
+// type Props = {
+//   fontColor?: string;
+//   // variable sin cammel case por warning en la consola, no encuentro el porque no sale el warninf en fontColor;
+//   widthpercent?: string;
+// };
+
+// const options = {
+//   shouldForwardProp: (prop: string) => prop !== "fontColor",
+//   shouldForwardProp2: (prop: string) => prop !== "widthpercent",
+// };
+
+// const StyledTextField = styled(
+//   TextField,
+//   options
+// )<Props>(({ fontColor, widthpercent }) => ({
+//   width: widthpercent,
+//   input: {
+//     color: fontColor,
+//   },
+// }));
 
 export default function Production() {
   return (
-    <Box>
-      <Grid container spacing={2} columnSpacing={4}>
-        <Grid item xs={8}>
-          <CardItemText>xs=8</CardItemText>
+    <Stack>
+      <Grid container spacing={3} mb={8} mt={2} style={centeredGrids}>
+        <Grid item xs={7}>
+          <Card style={cardBackgroundColor}>
+            <Box style={boxMargins}>
+              <Typography style={typographyStyles}>Pintor</Typography>
+              <TextField fullWidth></TextField>
+              <Typography style={typographyStyles}>Resinador</Typography>
+              <TextField fullWidth></TextField>
+              <Typography style={typographyStyles}>Fibrador</Typography>
+              <TextField fullWidth></TextField>
+              <Typography style={typographyStyles}>Manchador</Typography>
+              <TextField fullWidth></TextField>
+            </Box>
+          </Card>
         </Grid>
         <Grid item xs={4}>
-          <CardItemText>xs=4</CardItemText>
+          <Card style={cardBackgroundColor}>
+            <Box style={boxMargins}>
+              <Typography style={typographyStyles}>Fecha</Typography>
+              <TextField fullWidth></TextField>
+              <Typography style={typographyStyles}>Hora de inicio</Typography>
+              <TextField fullWidth></TextField>
+              <Typography style={typographyStyles}>Hora de termino</Typography>
+              <TextField fullWidth></TextField>
+            </Box>
+          </Card>
         </Grid>
       </Grid>
-    </Box>
+      <Grid container spacing={3} mb={3} style={centeredGrids}>
+        <Grid item xs={7}>
+          <Card style={cardBackgroundColor}>
+            <Box style={boxMargins}>
+              <Typography style={typographyStyles}>Tipo de molde</Typography>
+              <TextField fullWidth></TextField>
+              <Box style={checkboxesStyles}>
+                <Typography>Molde 1</Typography>
+                <Checkbox />
+                <Typography>Molde 2</Typography>
+                <Checkbox />
+              </Box>
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card style={cardBackgroundColor}>
+            <Box style={boxMargins}>
+              <Typography style={typographyStyles}>
+                Piezas fabricadas
+              </Typography>
+              <TextField fullWidth></TextField>
+              <Typography style={typographyStyles}>Numero de ciclo</Typography>
+              <TextField fullWidth></TextField>
+            </Box>
+          </Card>
+        </Grid>
+      </Grid>
+    </Stack>
   );
 }
